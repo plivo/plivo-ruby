fd122a5efef17110ed0bbd06977b7bd70d59bc3b
Alex Sharp <ajsharp@gmail.com>
2013-10-24 08:05:18 -0700

    better inline docs for AvailableNumberGroup.search

--- lib/plivo/available_number_group.rb
+++ lib/plivo/available_number_group.rb
@@ -2,6 +2,25 @@ module Plivo
 
   # @see http://plivo.com/docs/api/numbers/availablenumbergroup/
   class AvailableNumberGroup < Resource
+
+    # Search for new numbers.
+    #
+    # @see http://plivo.com/docs/api/numbers/availablenumbergroup/#number_search
+    #
+    # @param [Hash] opts
+    # @option opts [String] country_iso two letter country code. E.g. "US", "UK"
+    #   Required.
+    # @option opts [String] number_type 'local', 'national' or 'tollfree'.
+    #   Defaults to 'local'.
+    # @option opts [String] services comma-separated list of features. Set to
+    #   'voice', 'sms' or 'voice,sms' to search numbers with the respective
+    #   features available.
+    # @option opts [String] region Region in which to search for numbers.
+    #   Both region codes ('NY', 'CA') or region names ('New York', 'California')
+    #   may be used. Only applicable for 'local' numbers.
+    # @option opts [String] prefix A number prefix to filter by. Max length
+    #   of 5 digits.
+    #
     # @return [ResourceCollection]
     def self.search(opts = {})
       ResourceCollection.new(client.get_number_group(opts), self)
