ebb22bda8941635b05ba36daea3711c5a6b10010
Alex Sharp <ajsharp@gmail.com>
2013-10-23 19:17:31 -0700

    add support for looking up a rented number by the number

--- lib/plivo/rented_number.rb
+++ lib/plivo/rented_number.rb
@@ -9,5 +9,15 @@ module Plivo
     def unrent
       client.request('DELETE', uri)
     end
+
+    # Find a RentedNumber by the number instead of a uri
+    #
+    # @see http://plivo.com/docs/api/numbers/number/#number_detail
+    #
+    # @param [String] number a phone number
+    # @return [Plivo::RentedNumber]
+    def self.find(number)
+      super("Number/#{number}/")
+    end
   end
 end
--- spec/plivo/rented_number_spec.rb
+++ spec/plivo/rented_number_spec.rb
@@ -6,3 +6,13 @@ describe Plivo::RentedNumber, '#unrent', :vcr do
     num.unrent.status.should == 204
   end
 end
+
+describe Plivo::RentedNumber, '.find', :vcr do
+  it 'supports using a purchased phone number instead of a URI' do
+    group = Plivo::AvailableNumberGroup.search(country_iso: 'US')
+    num = group.first
+    payload = num.rent
+    purchased_number = payload['numbers'].first['number']
+    Plivo::RentedNumber.find(purchased_number).number.should == purchased_number
+  end
+end
