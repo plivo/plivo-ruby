587e4ca5d67466dee8a9c345691733fcc4db24f2
tsudot <kunalkerkar@gmail.com>
2015-03-13 16:14:00 +0530

    Add beep parameter to Wait element. Fixes #29

--- lib/plivo.rb
+++ lib/plivo.rb
@@ -648,7 +648,7 @@ module Plivo
 
     class Wait < Element
         @nestables = []
-        @valid_attributes = ['length', 'silence', 'min_silence']
+        @valid_attributes = ['length', 'silence', 'min_silence', 'beep']
 
         def initialize(attributes={})
             super(nil, attributes)
