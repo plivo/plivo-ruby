43d660a0bf2206d97ed5408403ce3b3399d4c4af
Kritarth <kritarth@plivo.com>
2020-04-28 10:28:23 +0530

    add jwt examples

---
+++ examples/jwt.rb
@@ -0,0 +1,26 @@
+require 'rubygems'
+require 'plivo'
+
+include Plivo
+
+AUTH_ID = 'MADADADADADADADADADA'
+AUTH_TOKEN = 'AUTH_TOKEN'
+
+begin
+  acctkn = Plivo::Token::AccessToken.new(
+    AUTH_ID,
+    AUTH_TOKEN,
+    '{username}',
+    '{uid}'
+  )
+  # update token validity (from, lifetime, till)
+  acctkn.update_validity(Time.now, 300)
+  # add voice grants (incoming, outgoing)
+  acctkn.add_voice_grants(Plivo::Token::VoiceGrants.new(true, true))
+  puts acctkn.to_jwt
+rescue ValidationError => e
+  puts 'Exception: ' + e.message
+end
+
+# Sample Response:
+#
