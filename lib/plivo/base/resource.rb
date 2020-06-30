366b4527acc0300a289a2267e642b4222fc2bf3a
Koushik-Ayila <koushik.ayila@plivo.com>
2020-06-30 15:20:29 +0530

    Dev Testing and XML done, TBD : UTs for SDK

--- lib/plivo/base/resource.rb
+++ lib/plivo/base/resource.rb
@@ -65,7 +65,6 @@ module Plivo
       end
 
       def perform_action(action = nil, method = 'GET', params = nil, parse = false)
-        puts params
         resource_path = action ? @_resource_uri + action + '/' : @_resource_uri
         response = @_client.send_request(resource_path, method, params,nil,false,is_voice_request: @_is_voice_request)
         parse ? parse_and_set(response) : self
