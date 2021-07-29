e502c8ec6574753a287f0e7bf063de602c38a69c
LSAITHARUN <saitharunreddylokireddy@gmail.com>
2021-07-29 12:25:56 +0530

    Removed nestables for Cont class

--- lib/plivo/xml/cont.rb
+++ lib/plivo/xml/cont.rb
@@ -1,7 +1,7 @@
 module Plivo
     module XML
       class Cont < Element
-        @nestables = %w(Break Emphasis Lang P Phoneme Prosody S SayAs Sub W)
+        @nestables = []
         @valid_attributes = []
   
         def initialize(body)
