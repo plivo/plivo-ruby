8ef47ba7feea3fa62450237ca6866a8d92b02772
Abinaya-plivo <abinaya.s@plivo.com>
2021-11-16 18:58:47 +0530

    removed code changes of start recording and stop recording announcements on MPC

--- lib/plivo/resources/multipartycalls.rb
+++ lib/plivo/resources/multipartycalls.rb
@@ -60,11 +60,7 @@ module Plivo
                           enter_sound='beep:1',
                           enter_sound_method='GET',
                           exit_sound='beep:2',
-                          exit_sound_method='GET',
-                          start_recording_audio=nil,
-                          start_recording_audio_method='GET',
-                          stop_recording_audio=nil,
-                          stop_recording_audio_method='GET')
+                          exit_sound_method='GET')
         if (from and to) and call_uuid
           raise_invalid_request('cannot specify call_uuid when (from, to) is provided')
         end
@@ -121,10 +117,6 @@ module Plivo
         valid_param?(:enter_sound_method, enter_sound_method.upcase, String, false, %w[GET POST])
         is_one_among_string_url?(:exit_sound, exit_sound, false , %w[beep:1 beep:2 none])
         valid_param?(:exit_sound_method, exit_sound_method.upcase, String, false, %w[GET POST])
-        valid_param?(:start_recording_audio_method, start_recording_audio_method.upcase, String, false, %w[GET POST])
-        valid_url?(:start_recording_audio, start_recording_audio, false ) unless start_recording_audio.nil?
-        valid_param?(:stop_recording_audio_method, stop_recording_audio_method.upcase, String, false, %w[GET POST])
-        valid_url?(:stop_recording_audio, stop_recording_audio, false ) unless stop_recording_audio.nil?
         if (to!=nil) && (ring_timeout.is_a?(String)) && (to.split('<').size < ring_timeout.split('<').size)
           raise_invalid_request("RingTimeout:number of ring_timout(s) should be same as number of destination(s)")
         end
@@ -176,10 +168,6 @@ module Plivo
         params[:enter_sound_method] = enter_sound_method.upcase unless exit_sound_method.nil?
         params[:exit_sound] = exit_sound unless exit_sound.nil?
         params[:exit_sound_method] = exit_sound_method.upcase unless exit_sound_method.nil?
-        params[:start_recording_audio] = start_recording_audio unless start_recording_audio.nil?
-        params[:start_recording_audio_method] = start_recording_audio_method.upcase unless start_recording_audio_method.nil?
-        params[:stop_recording_audio] = stop_recording_audio unless stop_recording_audio.nil?
-        params[:stop_recording_audio_method] = stop_recording_audio_method.upcase unless stop_recording_audio_method.nil?
         perform_action_apiresponse('Participant', 'POST', params, true )
       end
 
@@ -446,8 +434,6 @@ module Plivo
         options[:enter_sound_method] = 'GET' unless options.key?(:enter_sound_method)
         options[:exit_sound] = 'beep:2' unless options.key?(:exit_sound)
         options[:exit_sound_method] = 'GET' unless options.key?(:exit_sound_method)
-        options[:start_recording_audio_method] = 'GET' unless options.key?(:start_recording_audio_method)
-        options[:stop_recording_audio_method] = 'GET' unless options.key?(:stop_recording_audio_method)
         valid_param?(:friendly_name, options[:friendly_name], String, false) unless options[:friendly_name].nil?
         valid_param?(:uuid, options[:uuid], String, false) unless options[:uuid].nil?
         mpc_id = make_mpc_id(options[:uuid], options[:friendly_name])
@@ -457,8 +443,7 @@ module Plivo
                                                                           options[:wait_music_method],options[:agent_hold_music_url],options[:agent_hold_music_method],options[:customer_hold_music_url],options[:customer_hold_music_method],
                                                                           options[:recording_callback_url],options[:recording_callback_method],options[:status_callback_url],options[:status_callback_method],options[:on_exit_action_url], options[:on_exit_action_method],
                                                                           options[:record],options[:record_file_format],options[:status_callback_events],options[:stay_alone], options[:coach_mode],options[:mute],options[:hold],options[:start_mpc_on_enter],options[:end_mpc_on_exit],
-                                                                          options[:relay_dtmf_inputs],options[:enter_sound],options[:enter_sound_method],options[:exit_sound],options[:exit_sound_method], options[:start_recording_audio], options[:start_recording_audio_method],
-                                                                                                            options[:stop_recording_audio], options[:stop_recording_audio_method])
+                                                                          options[:relay_dtmf_inputs],options[:enter_sound],options[:enter_sound_method],options[:exit_sound],options[:exit_sound_method])
       end
 
       def start(options = {})
