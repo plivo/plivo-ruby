986c3bf6a1afc987a66b06ffc81cc8ba200e557e
kalyan-plivo <kalyan.sunku@plivo.com>
2023-11-08 13:07:28 +0530

    correcte the example file

--- examples/tollfree_verification.rb
+++ examples/tollfree_verification.rb
@@ -1,13 +1,16 @@
 require 'rubygems'
-require "/usr/src/app/lib/plivo.rb"
+require "plivo"
 
 include Plivo
 include Plivo::Exceptions
 
-api = RestClient.new("MAYJLIZGQ5MWVKZWM4NZ","MjFkZjQ3MzhiODFlZmY4MjIyODk2NTgxOGVmMDdm")
+AUTH_ID = 'AUTH_ID'
+AUTH_TOKEN = 'AUTH_TOKEN'
+
+api = RestClient.new(AUTH_ID, AUTH_TOKEN)
 
 begin
-  response = api.tollfree_verifications.get('871f1ec4-5280-44ad-6c9c-82478a646e03')
+  response = api.tollfree_verifications.get('uuid')
       puts response
 rescue PlivoRESTError => e
     puts 'Exception: ' + e.message
@@ -15,14 +18,16 @@ end
 
 
 begin
-  response = api.tollfree_verifications.create('18773582986', '2FA', 'dasas', 'd5a9c4d4-d086-42db-940f-e798d480d064', 'VERBAL', 'https://www.aa.com,http://google.com', '100', 'qwqwq')
+  response = api.tollfree_verifications.create('18773582986', '2FA', 'dasas', 
+    'd5a9c4d4-d086-42db-940f-e798d480d064', 'VERBAL', 'https://www.aa.com,http://google.com', 
+    '100', 'qwqwq')
       puts response
 rescue PlivoRESTError => e
     puts 'Exception: ' + e.message
 end
 
 begin
-response = api.tollfree_verifications.update('871f1ec4-5280-44ad-6c9c-82478a646e03', usecase: '2FA')
+response = api.tollfree_verifications.update('uuid', usecase: '2FA')
     puts response
 rescue PlivoRESTError => e
   puts 'Exception: ' + e.message
@@ -30,7 +35,7 @@ end
 
 
 begin
-response = api.tollfree_verifications.delete('871f1ec4-5280-44ad-6c9c-82478a646e03')
+response = api.tollfree_verifications.delete('uuid')
     puts response
 rescue PlivoRESTError => e
   puts 'Exception: ' + e.message
